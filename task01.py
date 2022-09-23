0eaed9ec67f743a161e814c1b0c2cb2efb8cfdef
macaljan <macaljan@fel.cvut.cz>
2022-09-24 01:07:37 +0200

    Commit changes from PyCharm on PC

--- task01.py
+++ task01.py
@@ -14,12 +14,19 @@ H = np.array([[1,     0.1,   0],
 plt.plot([1, 800, 800, 1, 1], [1, 1, 600, 600, 1], "k")
 # plt.axis([0, 900, 0, 700])
 
-points = plt.ginput(5)
+points = plt.ginput(4)
 
-for i in range(4):
+for i in range(2):
+    plt.plot(points[i][0], points[i][1], "or")
+    print(points[i])
+
+plt.plot([points[0][0], points[1][0]], [points[0][1], points[1][1]], "r-")
+
+for i in range(2, 4, 1):
     plt.plot(points[i][0], points[i][1], "ob")
     print(points[i])
 
-plt.plot(200, 200, "r")
+plt.plot([points[2][0], points[3][0]], [points[2][1], points[3][1]], "b-")
 
 plt.show()
+
