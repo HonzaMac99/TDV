257e664bd8cca6350c4b386a1624346428b100aa
macaljan <macaljan@fel.cvut.cz>
2022-09-26 01:48:10 +0200

    Commit changes from PyCharm on PC

--- task01.py
+++ task01.py
@@ -5,27 +5,24 @@ import matplotlib.pyplot as plt
 # plt.ginput ... for entering the points
 # np.cross ... cross product
 
-# test for changes
-
+# homography
 H = np.array([[1,     0.1,   0],
               [0.1,   1,     0],
               [0.004, 0.002, 1]])
 
+# plot the image boundary
 plt.plot([1, 800, 800, 1, 1], [1, 1, 600, 600, 1], "k")
-# plt.axis([0, 900, 0, 700])
 
 points = plt.ginput(4)
 
-for i in range(2):
-    plt.plot(points[i][0], points[i][1], "or")
+for i in range(4):
+    if i < 2:
+        plt.plot(points[i][0], points[i][1], "or")
+    else:
+        plt.plot(points[i][0], points[i][1], "ob")
     print(points[i])
 
 plt.plot([points[0][0], points[1][0]], [points[0][1], points[1][1]], "r--")
-
-for i in range(2, 4, 1):
-    plt.plot(points[i][0], points[i][1], "ob")
-    print(points[i])
-
 plt.plot([points[2][0], points[3][0]], [points[2][1], points[3][1]], "b--")
 
 plt.show()
