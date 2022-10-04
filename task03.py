d73b75d1150847d00c6c17521e6155135f08aea8
macaljan_WinPC <macaljan@fel.cvut.cz>
2022-10-04 20:01:08 +0200

    Commit changes from PyCharm on Windows PC

--- task03.py
+++ task03.py
@@ -3,35 +3,14 @@ import matplotlib.pyplot as plt
 import toolbox as tb
 
 
-def find_start_end(line):
-    start = np.zeros([2,])
-    end = np.zeros([2,])
-    j = 0
-    for i in range(2):
-        while line[j] == ' ':
-            j += 1
-        start[i] = j
-
-        while line[j] != ' ' and line[j] != '\n':
-            j += 1
-        end[i] = j
-
-    return start, end
+# rng = np.random.default_rng()
+# i = rng.choice(n, 2, replace=False)
 
 
-def read_points(fname):
-    file = open(fname, "r")
-    points = []
-    for line in file:
-        start, end = find_start_end(line)
-        point_x = float(line[start[0]:end[0]])
-        point_y = float(line[start[1]:end[1]])
-        points.append([point_x, point_y])
+x = np.loadtxt("linefit_1.txt").T
+x = np.array(x)
 
-    return np.array(points)
-
-
-data = read_points("linefit_1.txt")
-print(data)
+plt.plot(x[0], x[1], 'ko', markersize=2)
+plt.show()
 
 
