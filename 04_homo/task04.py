b8ef62f4b4263a2d42dc7744c2ceb6af55eec393
macaljan_LinPC <macaljan@fel.cvut.cz>
2023-10-31 21:59:23 +0100

    Cleaned the code of task04

--- 04_homo/task04.py
+++ 04_homo/task04.py
@@ -8,7 +8,9 @@ import math
 import copy
 import sys
 
+
 def get_line_boundaries(plot_line, img):
+    plot_line = plot_line.reshape(1, 3)
     min_x = 1
     min_y = 1
     max_x = img.shape[1]
@@ -38,83 +40,83 @@ def get_line_boundaries(plot_line, img):
             count += 1
     return line_boundaries
 
+
 # show the images and their feature points
-def plot_features(img1, img2, features1, features2):
+def plot_features(img1, img2, fs1, fs2):
     fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.imshow(img1)
-    ax1.scatter(features1[:, 0], features1[:, 1], s=0.5, c='black')
-    ax1.set_title("book1.png")
+    ax1.imshow(img2)
+    ax1.scatter(fs2[:, 0], fs2[:, 1], s=0.5, c='black')
+    ax1.set_title("book2.png")
 
-    ax2.imshow(img2)
-    ax2.scatter(features2[:, 0], features2[:, 1], s=0.5, c='black')
-    ax2.set_title("book2.png")
+    ax2.imshow(img1)
+    ax2.scatter(fs1[:, 0], fs1[:, 1], s=0.5, c='black')
+    ax2.set_title("book1.png")
 
     plt.show()
 
 
 # show the differences between both feature groups on the images
-def plot_corresp(img1, img2, features1, features2, corresp):
+def plot_corresp(img1, img2, fs1, fs2, corresp):
     fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.imshow(img1)
+    ax1.imshow(img2)
     for i in range(corresp.shape[0]):
         idx1 = corresp[i, 0]
         idx2 = corresp[i, 1]
-        ax1.plot([features1[idx1, 0], features2[idx2, 0]],
-                 [features1[idx1, 1], features2[idx2, 1]], color='green')
-    ax1.scatter(features1[:, 0], features1[:, 1], marker='o', s=0.8, c='black')
+        ax1.plot([fs1[idx1, 0], fs2[idx2, 0]],
+                 [fs1[idx1, 1], fs2[idx2, 1]], color='green')
+    ax1.scatter(fs1[:, 0], fs1[:, 1], marker='o', s=0.8, c='black')
     ax1.set_title("book2.png")
 
-    ax2.imshow(img2)
+    ax2.imshow(img1)
     for i in range(corresp.shape[0]):
         idx1 = corresp[i, 0]
         idx2 = corresp[i, 1]
-        ax2.plot([features2[idx2, 0], features1[idx1, 0]],
-                 [features2[idx2, 1], features1[idx1, 1]], color='red')
-    ax2.scatter(features2[:, 0], features2[:, 1], marker='o', s=0.8, c='black')
+        ax2.plot([fs2[idx2, 0], fs1[idx1, 0]],
+                 [fs2[idx2, 1], fs1[idx1, 1]], color='red')
+    ax2.scatter(fs2[:, 0], fs2[:, 1], marker='o', s=0.8, c='black')
     ax2.set_title("book1.png")
 
     plt.show()
 
 
 # show the results of the homography estimation
-def plot_ha(img1, img2, features1, features2, best_points1, best_points2, best_H):
+def plot_ha(img1, img2, fs1, fs2, bp1, bp2, Ha):
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    best_H_inv = np.linalg.inv(best_H)
+    Ha_inv = np.linalg.inv(Ha)
 
     # ax1.imshow(img1)
     ax1.set_title("Projected to img 1")
 
-    ax1.scatter(features1[:, 0], features1[:, 1], s=0.5, c='black')
-    ax1.scatter(features2[:, 0], features2[:, 1], s=0.2, c='gray')
-    f2_proj = tb.p2e(best_H_inv@tb.e2p(features2.T)).T
-    ax1.scatter(f2_proj[:, 0], f2_proj[:, 1], s=0.2, c='red')
+    ax1.scatter(fs1[:, 0], fs1[:, 1], s=0.5, c='black')
+    ax1.scatter(fs2[:, 0], fs2[:, 1], s=0.2, c='gray')
+    fs2_proj = tb.p2e(Ha_inv@tb.e2p(fs2.T)).T
+    ax1.scatter(fs2_proj[:, 0], fs2_proj[:, 1], s=0.2, c='red')
 
     # plot the 4 points used for H estimation
-    ax1.scatter(best_points1[0, :], best_points1[1, :], s=30.0, c='black', label="Orig. features")
-    ax1.scatter(best_points2[0, :], best_points2[1, :], s=12.0, c='gray', label="Corresp. features")
-    bp2_proj = tb.p2e(best_H_inv@tb.e2p(best_points2)).T
+    ax1.scatter(bp1[0, :], bp1[1, :], s=30.0, c='black', label="Orig. features")
+    ax1.scatter(bp2[0, :], bp2[1, :], s=12.0, c='gray', label="Corresp. features")
+    bp2_proj = tb.p2e(Ha_inv@tb.e2p(bp2)).T
     ax1.scatter(bp2_proj[:, 0], bp2_proj[:, 1], s=12.0, c='red', label="Projected corresp. features")
 
     ax1.legend()
 
     # ax2.imshow(img2)
     ax2.set_title("Projected to img 2")
-    ax2.scatter(features2[:, 0], features2[:, 1], s=0.5, c='black')
-    ax2.scatter(features1[:, 0], features1[:, 1], s=0.2, c='gray')
-    f1_proj = tb.p2e(best_H@tb.e2p(features1.T)).T
-    ax2.scatter(f1_proj[:, 0], f1_proj[:, 1], s=0.2, c='green')
+    ax2.scatter(fs2[:, 0], fs2[:, 1], s=0.5, c='black')
+    ax2.scatter(fs1[:, 0], fs1[:, 1], s=0.2, c='gray')
+    fs1_proj = tb.p2e(Ha@tb.e2p(fs1.T)).T
+    ax2.scatter(fs1_proj[:, 0], fs1_proj[:, 1], s=0.2, c='green')
 
     # plot the 4 points used for H estimation
-    ax2.scatter(best_points2[0, :], best_points2[1, :], s=30.0, c='black', label="Orig. features")
-    ax2.scatter(best_points1[0, :], best_points1[1, :], s=12.0, c='gray', label="Corresp. features")
-    bp1_proj = tb.p2e(best_H@tb.e2p(best_points1)).T
+    ax2.scatter(bp2[0, :], bp2[1, :], s=30.0, c='black', label="Orig. features")
+    ax2.scatter(bp1[0, :], bp1[1, :], s=12.0, c='gray', label="Corresp. features")
+    bp1_proj = tb.p2e(Ha@tb.e2p(bp1)).T
     ax2.scatter(bp1_proj[:, 0], bp1_proj[:, 1], s=12.0, c='green', label="Projected corresp features")
 
     ax2.legend()
     plt.show()
-
 
 
 book1 = mpimg.imread('book1.png')
@@ -123,28 +125,26 @@ features1 = np.genfromtxt('books_u1.txt', dtype='float')
 features2 = np.genfromtxt('books_u2.txt', dtype='float')
 corresp = np.genfromtxt('books_m12.txt', dtype='int')
 
-
 # show the images and their feature points
-# plot_features(book2, book1, features2, features1)
+# plot_features(book1, book2, features1, features2)
 
 # show the differences between both feature groups on the images
-# plot_corresp(book2, book1, features2, features1, corresp)
+# plot_corresp(book1, book2, features1, features2, corresp)
 
 
 # -------------------------------------- estimate the first homography Ha -----------------------------------------
 print("Estimating Ha")
+
 best_Ha = np.zeros((3, 3))
 best_points1 = np.zeros((2, 4))
 best_points2 = np.zeros((2, 4))
-inlier_idxs = []
+best_support = 0
 best_inlier_idxs = []
 
 rng = np.random.default_rng()
 n_crp = corresp.shape[0]
 k = 0
 k_max = 100
-support = 0
-best_support = 0
 theta = 3  # pixels
 probability = 0.99
 while k <= k_max:
@@ -182,8 +182,8 @@ while k <= k_max:
 
     if support > best_support:
         best_support = support
-        best_inlier_idxs = inlier_idxs
         best_Ha = Ha
+        best_inlier_idxs = inlier_idxs
         best_points1 = points1
         best_points2 = points2
         print("[ k:", k, "/", k_max, "] [ support:", support, "/", n_crp, "]")
@@ -194,8 +194,8 @@ while k <= k_max:
 
 print("[ k:", k-1, "/", k_max, "] [ support:", best_support, "/", n_crp, "]")
 Ha = best_Ha
-inlier_idxs = best_inlier_idxs
 Ha_inv = np.linalg.inv(Ha)
+inlier_idxs = best_inlier_idxs
 
 # plot the results of the first (dominant) homography estimation
 # plot_ha(book1, book2, features1, features2, best_points1, best_points2, best_Ha)
@@ -213,18 +213,17 @@ corresp = np.array(new_corresp)
 
 # -------------------------------------- estimate the homology and second homography Hb -----------------------------------------
 print("Estimating Hb")
+
 best_Hb = np.zeros((3, 3))
 best_points1 = np.zeros((2, 3))
 best_points2 = np.zeros((2, 3))
-inlier_idxs = []
+best_support = 0
 a = np.zeros((1, 3))
 
 rng = np.random.default_rng()
 n_crp = corresp.shape[0]
 k = 0
 k_max = 1000
-support = 0
-best_support = 0
 theta = 3  # pixels
 probability = 0.99
 while k <= k_max:
@@ -239,7 +238,7 @@ while k <= k_max:
     [u1, u2, u3] = tb.e2p(points1).T
     [u_1, u_2, u_3] = points2_proj.T  # "_" represents " ' "
 
-    v = np.cross(np.cross(u1, u_1), np.cross(u2, u_2))
+    v = np.cross(np.cross(u1, u_1), np.cross(u2, u_2)).reshape((3, 1))
 
     A = np.vstack(((u_1[0]*v[2] - u_1[2]*v[0])*u1,
                    (u_2[0]*v[2] - u_2[2]*v[0])*u2,
@@ -249,22 +248,12 @@ while k <= k_max:
                   u2[0]*u_2[2] - u2[2]*u_2[0],
                   u3[0]*u_3[2] - u3[2]*u_3[0]])
 
-    # # solve Aa = b
-    # a = np.linalg.solve(A, b)
-    A_inv = np.linalg.inv(A)
-    a = A_inv@b
+    # solve Aa = b
+    a = np.linalg.solve(A, b).reshape((3, 1))
 
-    H = np.eye(3) + v.reshape(3, 1)@a.reshape((1, 3))
-
+    H = np.eye(3) + v@a.T
     Hb = Ha@H
     Hb_inv = np.linalg.inv(Hb)
-
-    # fig, ax = plt.subplots()
-    # ax.imshow(book1)
-    # a1 = tb.p2e(a.reshape(3, 1))
-    # a2 = tb.p2e(a.reshape(3, 1))
-    # ax.plot([a1[0], a2[0]], [a1[1], a2[1]], "v--")
-    # plt.show()
 
     support = 0
     inlier_idxs = []
@@ -281,8 +270,8 @@ while k <= k_max:
 
     if support > best_support:
         best_support = support
-        best_inlier_idxs = inlier_idxs
         best_Hb = Hb
+        best_inlier_idxs = inlier_idxs
         best_points1 = points1
         best_points2 = points2
         print("[ k:", k, "/", k_max, "] [ support:", support, "/", n_crp, "]")
@@ -293,8 +282,8 @@ while k <= k_max:
 
 print("[ k:", k-1, "/", k_max, "] [ support:", best_support, "/", n_crp, "]")
 Hb = best_Hb
-inlier_idxs = best_inlier_idxs
 best_H_inv = np.linalg.inv(Hb)
+inlier_idxs = best_inlier_idxs
 
 # create corresp array without current inliers
 outliers = []
